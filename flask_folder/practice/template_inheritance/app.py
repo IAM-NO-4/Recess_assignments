993237f75e8c13a10d9149b52ef71d2dab648004
Ibrahim Amir muwayire <muwayirei@gmail.com>
2026-07-15 21:14:44 +0300

    first django

--- flask_folder/practice/template_inheritance/app.py
+++ flask_folder/practice/template_inheritance/app.py
@@ -1,9 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,flash,request,redirect,url_for
+import random
 app = Flask(__name__)
+x = random.randint(1,192666666666666)
+app.secret_key = str(x)
 
 @app.route("/index")
 def student():
     return render_template("index.html")
+
+@app.route("/login",methods= ["POST","get"])
+def register():
+    if request.method == "POST":
+        flash("login successful","sucess")
+        return redirect(url_for('student'))
+    return render_template("form.html")
+
 
 
 if __name__ == "__main__":
